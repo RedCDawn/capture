3ffea0cb05432bd742b0a3fbeca07335938e72e2
RedCDawn <chris.ducastel@gmail.com>
2020-12-13 18:40:40 +0100

    6. Styling Services Section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import home1 from "../img/home1.png";
 //Styles
-import styled from "styled-components";
+//import styled from "styled-components";
+import { BasicLayout, Description, Image, Hide } from "../styles.jsx";
 
 const AboutSection = () => {
   return (
-    <About>
+    <BasicLayout>
       <Description>
         <div className="title">
           <Hide>
@@ -29,40 +30,8 @@ const AboutSection = () => {
       <Image>
         <img src={home1} alt="guy with a camera" />
       </Image>
-    </About>
+    </BasicLayout>
   );
 };
-
-// Styled Components
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
