bcd2cc83733111e2de4108d7a8bea45459e93220
RedCDawn <chris.ducastel@gmail.com>
2020-12-14 13:46:20 +0100

    9. Our Work Page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@
 import GlobalStyle from "./components/GlobalStyle";
 // Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs /> 
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route>
+          <ContactUs path="/contact"/>
+        </Route>
+      </Switch> 
     </div>
   );
 }
